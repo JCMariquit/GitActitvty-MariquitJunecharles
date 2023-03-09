42f78fbf8b6a04a0903b3d305762ca98dcee542d
MariquitJunecharles <junecharlesmariquit553@gmail.com>
2023-03-09 13:05:19 +0800

    My Hobbies added

--- git-python-Mariquit.py
+++ git-python-Mariquit.py
@@ -8,6 +8,8 @@ Created on Wed Mar  8 12:03:25 2023
 def personal_details():
     print('╔═══════════════════════ ◤◢◣◥◤◢◣◆◢◣◥◤◢◣◥ ════════════════════════╗')
     print('')
+    print('                        My Personal Info')
+    print('')
     name = "               June charles mariquit"
     address = "            Nangka II Mogpog, Marinduque"
     birthday = "           June 18. 2002"
@@ -20,6 +22,8 @@ personal_details()
 def personal_details():
     print('╔═══════════════════════ ◤◢◣◥◤◢◣◆◢◣◥◤◢◣◥ ════════════════════════╗')
     print('')
+    print('                    My Educational Background')
+    print('')
     school = "               Marinduque State College"
     CYS = "            BSIT 2C"
     print("School: {}\nCourse/Year/Section: {}".format(school, CYS))
@@ -27,3 +31,19 @@ def personal_details():
     print('╚═══════════════════════ ◣◥◤◢◣◥◤◆◥◤◢◣◥◤◢ ════════════════════════╝')
     
 personal_details()
+
+def personal_details():
+    print('╔═══════════════════════ ◤◢◣◥◤◢◣◆◢◣◥◤◢◣◥ ════════════════════════╗')
+    print('')
+    print('                          My Hobbies')
+    print('')
+    print('Hobbies:                   Cycling       ')
+    print('                     Playing Online games')
+    print('                        Watching Movies')
+    print('                           Sleeping')
+    print('                            Hiking ')
+    print('')
+    print('╚═══════════════════════ ◣◥◤◢◣◥◤◆◥◤◢◣◥◤◢ ════════════════════════╝')
+    
+personal_details()
+
