f6be51050345d4a106125958dca6f74b24c2790e
MariquitJunecharles <junecharlesmariquit553@gmail.com>
2023-03-09 12:40:33 +0800

    My Personal Info added

---
+++ git-python-Mariquit.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 12:03:25 2023
+
+@author: huhuhu
+"""
+
+def personal_details():
+    print('╔═══════════════════════ ◤◢◣◥◤◢◣◆◢◣◥◤◢◣◥ ════════════════════════╗')
+    print('')
+    name = "               June charles mariquit"
+    address = "            Nangka II Mogpog, Marinduque"
+    birthday = "           June 18. 2002"
+    print("Name: {}\nAddress: {}\nBirthday: {}".format(name, address, birthday))
+    print('')
+    print('╚═══════════════════════ ◣◥◤◢◣◥◤◆◥◤◢◣◥◤◢ ════════════════════════╝')
+    
+personal_details()
